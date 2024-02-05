c1e11f735156e6680ce4b77d615f426f845efe02
blackzspace <blackzspace.de@outlook.de>
2024-02-05 10:15:36 +0100

    added

---
+++ push.sh
@@ -0,0 +1,7 @@
+echo "# ts" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/www-blackzspace-de/blackzspace.de-reactjs-typescript.git
+git push -u origin main
